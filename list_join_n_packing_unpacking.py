73d72156cb2782757a799a30ca34d95777569d77
saurafs <saurafsgaming@gmail.com>
2026-08-21 21:55:59 +0530

    Add files via upload

---
+++ list_join_n_packing_unpacking.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 17 18:43:55 2026
+
+@author: Saurabh
+"""
+
+list1=['aman','baba','chotu','lala','rishab']
+sentence='!' 
+print(sentence.join(list1)) #aman!baba!chotu!lala!rishab
+
+
+a,b,c,*others,d=[1,2,3,4,5,6,7,8]
+print(a)      #1
+print(b)      #2
+print(c)      #3
+print(others) #[4,5,6,7]
+print(d)      #8
