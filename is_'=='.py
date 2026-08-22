89e3b3219cb2a6e5e11122d5ac08774fc0a03078
saurafs <saurafsgaming@gmail.com>
2026-08-22 21:11:28 +0530

    Add files via upload

---
+++ is_'=='.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 22 20:48:21 2026
+
+@author: Saurabh
+"""
+
+print(True == 1)      #true
+print('N' == 1)       #false
+print([] ==1)         #false
+print(10 ==10.0)      #true
+print([1,2] == [1,2]) #true
+
+
+print(True is 1)       #false
+print('N' is 1)        #false
+print([] is 1)         #false
+print(10 is 10.0)      #false
+print([1,2] is [1,2])  #false
---
+++ truthy_falsy.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 22 19:48:23 2026
+
+@author: Saurabh
+
+truthy and falsy.
+"""
+# print(bool(False))    #false
+# print(bool(false))    #not defined
+# print(bool('false'))  #true --> string
+# print(bool(None))     #false
+# print(bool(''))       #false --> any empty value
+# print(bool(0))        #false --> any ttpe of zero
+
+
+'''other than that everthing true'''
