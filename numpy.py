54886e5d505257695992e16c1f9f4caa870888b3
saurafs <saurafsgaming@gmail.com>
2026-08-31 20:21:06 +0530

    Add files via upload

---
+++ numpy.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 31 19:04:39 2026
+
+@author: Saurabh
+"""
+
+#import numpy as np
+
+# np.random.seed(9995)
+# random_arr = np.random.randint(0,30, size=(5,5))
+# print(random_arr)
+
+# ar1 = np.zeros([2,2])
+# print(ar1)
+
+# print(np.unique(ar1))
+
+# Slice the array----------------------------
+# a = np.array([[[1, 2], [3, 4]], 
+#               [[5, 6], [7, 8]], 
+#               [[9, 10], [11, 12]]])
+
+# sliced = a[:2, :1, :1]
+# print(sliced)
+
+#DOT PRODUCT---------------------------------
+# matrix1 =np.array([[1,2,3],[4,5,6],[7,8,9]])   
+# matrix2 =np.array([[2,2,3],[4,4,4],[1,1,4]])
+
+# print(np.ndim(matrix1))
+# product = np.dot(matrix1,matrix2)
+# print(product)
+
+# mult = matrix1*matrix2
+# print(mult)
+
+# matrix3 =np.array([[1,2],[4,5],[7,8]])   
+# matrix4 =np.array([[2,2,3],[4,4,4]])
+# print(np.shape(matrix3))
+# print(np.shape(matrix4))
+# product = np.dot(matrix3,matrix4)
+# print(product)
+
+#TRANSPOSE AND RESHAPE------------------------
+# matrix5 =np.array([[1,2,3,7],[4,5,6,9],[7,8,9,3]])
+
+# re5 =matrix5.reshape(6,2) #reshape
+# print(np.shape(matrix5)) 
+# print(re5)
+
+# tra5= matrix5.T #transpose
+# print(tra5)
+
+#DISPLAY-------------------------------------
+# from IPython import display
+# display.Image("bakugou.jpg")
+
+# from matplotlib.image import imread
+# bakugo = imread("bakugou.jpg")
+
+# print(bakugo)
+# print(bakugo.shape)
+# print(bakugo.ndim)
+# print(bakugo.size)
+
+
