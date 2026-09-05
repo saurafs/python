87f63f4d040b59b47eb6158e78eee83ad4bdf1c2
saurafs <saurafsgaming@gmail.com>
2026-09-05 19:38:06 +0530

    Add files via upload

---
+++ plotly_express.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  5 17:47:21 2026
+
+@author: Saurabh
+"""
+
+import plotly.express as px
+import plotly.io as pio
+
+# df = px.data.stocks()
+
+# px.line(df, y='GOOG', x='date')
+# px.line(df, y=['GOOG','FB'], x='date')
+# px.line(df, y=['GOOG','FB'], x='date', range_x=['2019','2019-5'])
+# px.line(df, y=['GOOG','FB'], x='date', labels={'value':'close', 'date':'time'}, title="stocks")
+# px.line(df, y='GOOG', x='date', height=400)
+# px.line(df, y='GOOG', x='date', template='plotly_dark')
+
+# print(pio.templates)
